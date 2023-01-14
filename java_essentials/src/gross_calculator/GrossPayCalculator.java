48fae023b470916e4fed46913b9a0de9c696379d
Ramona_Ekanayake <ramona.ekanayake@appliedtechnology.se>
2023-01-14 17:39:04 +0100

    1-3 Chapters

---
+++ java_essentials/src/gross_calculator/GrossPayCalculator.java
@@ -0,0 +1,22 @@
+package gross_calculator;
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String[] args) {
+
+       int  hours = 0;
+       System.out.println("How many hours did you work?");
+
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
+
+       double payRate = 0;
+       System.out.println("Whats your pay rate?");
+       payRate = scanner.nextDouble();
+
+       scanner.close();
+
+       double grossPay = hours * payRate;
+       System.out.println("Your pay is" + " " + grossPay +"kr");
+    }
+}
